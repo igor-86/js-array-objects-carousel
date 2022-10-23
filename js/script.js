be11e4f367bf7eea8d1c28709240bf85c0ca94bc
igor-86 <i.campoli86@gmail.com>
2022-10-23 23:05:38 +0200

    Slider thumbs

--- js/script.js
+++ js/script.js
@@ -23,16 +23,18 @@ const images = [
     }
 ];
 //Catturo il container dove inserire gli oggetti nel array
-const containerImage = document.querySelector(".container");
-console.log(containerImage);
+const containerImage = document.querySelector(".box-items");
+//Thumbs contenitore
+const containerThumbs = document.querySelector(".thumbs");
+console.log(containerThumbs);
 
 images.forEach((courrentItems) => {
     const imageRound = courrentItems.image;
     const titleRound = courrentItems.title;
     const textRound = courrentItems.text;
-
+    
     containerImage.innerHTML += `
-        <div class="box-items">
+        <div class="item">
                 <img src="${imageRound}" alt="">
                 <div class="box-title">
                     <h3>${titleRound}</h3>
@@ -40,15 +42,30 @@ images.forEach((courrentItems) => {
                 </div>
         </div>
     `;
+
+    containerThumbs.innerHTML += `
+        <div class="thumb">
+            <img src="${imageRound}" alt="">
+        </div> 
+    `;
+
+  
 });
 
+
+
+
+
 //Preparo lo Slider nella posizione iniziale
-const sliderItems = document.getElementsByClassName("box-items");
+const sliderItems = document.getElementsByClassName("item");
+const sliderThumb = document.getElementsByClassName("thumb");
+
 const buttonNext = document.querySelector(".next");
 const buttonPrev = document.querySelector(".prev");
 
 let activeItem = 0;
 sliderItems[activeItem].classList.add("active");
+sliderThumb[activeItem].classList.add("active-thumb");
 /* ____________________________________ */
 
 //Event next
@@ -63,6 +80,7 @@ buttonPrev.addEventListener("click", prevSlider);
  */
 function nextSlider(){
     sliderItems[activeItem].classList.remove("active");
+    sliderThumb[activeItem].classList.remove("active-thumb");
 
     if(activeItem < sliderItems.length - 1){
         activeItem++;
@@ -71,10 +89,12 @@ function nextSlider(){
     };
 //Aggiungo active item
     sliderItems[activeItem].classList.add("active");
+    sliderThumb[activeItem].classList.add("active-thumb");
 }
 
 function prevSlider(){
     sliderItems[activeItem].classList.remove("active");
+    sliderThumb[activeItem].classList.remove("active-thumb");
 
     if(activeItem > 0){
         activeItem--;
@@ -83,5 +103,23 @@ function prevSlider(){
     }
 
     sliderItems[activeItem].classList.add("active");
+    sliderThumb[activeItem].classList.add("active-thumb");
+}
+
+//Aggiungere slider al check su thumb
+for (let i = 0; i < sliderThumb.length; i++) {
+    const thisThumb = sliderThumb[i];
+    thisThumb.addEventListener("click", function() {
+        // Cancellare active da slider item e dal thumb
+        sliderItems[activeItem].classList.remove("active");
+        sliderThumb[activeItem].classList.remove("active-thumb");
+
+        // Aggiornare la posizione attuale
+        activeItem = i;
+
+        // Aggiungere active alla nuova posizione dell'immagine e del thumb
+        sliderItems[activeItem].classList.add("active");
+        sliderThumb[activeItem].classList.add("active-thumb");
+    });
 }
 
